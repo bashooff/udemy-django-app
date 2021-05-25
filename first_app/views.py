588e6dc432404c52fc13d080b9988b7b14fc01a0
Bas Hooff <me@bashooff.com>
2021-05-25 09:04:58 +0200

    added table to index
    Added visible table to the index page

--- first_app/views.py
+++ first_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Topic, Webpage, AccessRecord
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'first_app/index.html')
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records':webpages_list}
+    return render(request, 'first_app/index.html', context=date_dict)
